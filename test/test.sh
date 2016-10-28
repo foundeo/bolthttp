239ec81ffb019a06a92a4651dfa927573441153a
Pete Freitag <pete@foundeo.com>
2016-10-28 13:29:12 -0400

    Add verbose option to curl

--- test/test.sh
+++ test/test.sh
@@ -3,7 +3,7 @@
 cd `dirname $0`
 CWD="`pwd`"
 
-curl http://127.0.0.1:8484/test/run.cfm?reporter=text
+curl --verbose "http://127.0.0.1:8484/test/run.cfm?reporter=text"
 
 exitcode=$(<.exitcode)
 rm -f .exitcode
